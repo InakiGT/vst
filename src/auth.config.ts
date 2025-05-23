f674bdd9e4efb6517b1a5c885a97bc074853ada0
InakiGT <yarimfuego@gmail.com>
2025-05-23 14:19:41 -0600

    [UPDATED] base path on login callbacks

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -2,13 +2,13 @@ import type { NextAuthConfig } from 'next-auth'
 
 export const authConfig = {
   pages: {
-    signIn: '/login',
+    signIn: '/vst/login',
   },
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user
 
-      const isOnDashboard = nextUrl.pathname.startsWith('/itinerary')
+      const isOnDashboard = nextUrl.pathname.startsWith('/vst/itinerary')
       if (isOnDashboard) {
         if (isLoggedIn) return true
         return false

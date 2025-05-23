0b75cebe57bceab0aea8bc3d53287d17d0bc8aa5
InakiGT <yarimfuego@gmail.com>
2025-05-23 14:31:59 -0600

    [UPDATED] Images base path

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   basePath: '/vst',
   assetPrefix: '/vst',
+  images: {
+    loader: 'default',
+    path: '/vst/_next/image',
+  },
 };
 
 export default nextConfig;
